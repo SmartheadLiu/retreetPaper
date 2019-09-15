7ba2b8ba8070de9e938694656fcb09eb87cb5edd
Yanjun Wang <wang3204@purdue.edu>
2019-09-14 23:50:27 -0400

    Extract some sets, working on extraction.

--- src/Retreet.java
+++ src/Retreet.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import java.util.logging.Logger;
@@ -25,9 +26,50 @@ public class Retreet {
 			return;
 		}
 
-		// ParseTreeWalker walker = new ParseTreeWalker();
-        // RetreetListener listener = new RetreetListener();
-        // walker.walk(listener, tree);
+		ParseTreeWalker walker = new ParseTreeWalker();
+        RetreetExtractor listener = new RetreetExtractor();
+        walker.walk(listener, tree);
+
+        List<String> funcs = listener.getFuncs();
+        List<Block> blocks = listener.getBlocks();
+        Set<String> calls = listener.getCalls();
+        Set<String> noncalls = listener.getNoncalls();
+        Map<String, List<String>> funcBlock = listener.getFuncBlock();
+
+        System.out.println("Print out AllFuncs:");
+        for (String func : funcs) {
+        	System.out.println("Function: " + func);
+        }
+        System.out.println();
+
+        System.out.println("Print out AllBlocks:");
+        for (Block block : blocks) {
+        	System.out.println("Block id: " + block.getId() + " Text: " + block.getText());
+        }
+        System.out.println();
+
+        System.out.println("Print out AllCalls:");
+        for (String call : calls) {
+        	System.out.println("Call block id: " + call);
+        }
+        System.out.println();
+
+        System.out.println("Print out AllNoncalls:");
+        for (String noncall : noncalls) {
+        	System.out.println("Noncall block id: " + noncall);
+        }
+        System.out.println();
+
+        System.out.println("Print out mapping of functions to blocks:");
+        for (String func : funcBlock.keySet()) {
+        	List<String> blockList = funcBlock.get(func);
+        	System.out.println("Current function: " + func);
+        	for (String b : blockList) {
+        		System.out.println("	block: " + b);
+        	}
+        }
+        System.out.println();
+
 	}
 }
 
---
+++ src/RetreetExtractor.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class RetreetExtractor extends RetreetBaseListener {
+
+    List<String> funcs = new LinkedList<String>();		// AllFuncs
+    List<String> params = new LinkedList<String>();		// 
+    List<Block> blocks = new LinkedList<Block>();		// AllBlocks
+    Set<String> calls = new HashSet<String>();		// AllCalls: a set of ids of the call block
+    Set<String> noncalls = new HashSet<String>();	// AllNonCalls: a set of ids of the noncall block
+
+    Map<String, List<String>> funcBlock = new LinkedHashMap<String, List<String>>();		// Blocks(f)
+    Map<String, List<String>> funcParam = new LinkedHashMap<String, List<String>>();		// Params(f)
+
+    // Map<String, List<String>> read = new LinkedHashMap<String, List<String>>();		// read set
+    // Map<String, List<String>> write = new LinkedHashMap<String, List<String>>();		// write set
+
+    Block mainBlock; 		// should recognize which block is the main block; and the relationship between the calls in main and the functions
+    String currFunc;
+    String currBlock;
+
+    public List<String> getFuncs() {
+    	return this.funcs;
+    }
+
+    public List<Block> getBlocks() {
+    	return this.blocks;
+    }
+
+    public Set<String> getCalls() {
+    	return this.calls;
+    }
+
+    public Set<String> getNoncalls() {
+    	return this.noncalls;
+    }
+
+    public Map<String, List<String>> getFuncBlock() {
+    	return this.funcBlock;
+    }
+
+    public void enterMain(RetreetParser.MainContext ctx) {
+    	String name = ctx.getChild(0).getText();
+    	funcs.add(name);
+    	currFunc = name;
+    	List<String> blocksInFunc = new LinkedList<String>();
+    	funcBlock.put(currFunc, blocksInFunc);
+    }
+
+    public void enterFunc(RetreetParser.FuncContext ctx) {
+    	String name = ctx.getChild(0).getText();
+    	funcs.add(name);
+    	currFunc = name;
+    	List<String> blocksInFunc = new LinkedList<String>();
+    	funcBlock.put(currFunc, blocksInFunc);
+    }
+
+    public void enterBlock(RetreetParser.BlockContext ctx) {
+    	String blockid = Integer.toString(blocks.size());
+    	Block newblock = new Block(blockid, ctx.getText());
+    	if (ctx.call() != null) {
+    		newblock.setCall();
+    		calls.add(blockid);
+    	}
+    	if (ctx.assgn_list() != null) {
+    		newblock.setNoncall();
+    		noncalls.add(blockid);
+    	}
+    	blocks.add(newblock);
+    	// add this block id to the current function-block mapping
+    	List<String> blocksInFunc = funcBlock.get(currFunc);
+    	blocksInFunc.add(blockid);
+    	funcBlock.put(currFunc, blocksInFunc);
+    }
+
+}
